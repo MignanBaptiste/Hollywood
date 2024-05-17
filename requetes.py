cf722049923f48ba2b6c160fdf217f7a6b965a84
MignanBaptiste <baptiste.mignan@etu.univ-orleans.fr>
2024-05-17 13:51:14 +0200

    changement de tests

--- requetes.py
+++ requetes.py
@@ -6,7 +6,5 @@ def json_vers_nx(chemin):
     graph = nx.Graph()
     with open(chemin, "r") as file:
         for ligne in file:
-            acteurs = json.loads(ligne)["cast"]
-            acteurs = [acteur.strip("[").strip("]") for acteur in acteurs]
-            graph.add_edges_from(itertools.combinations(acteurs, 2))
+            graph.add_edges_from(itertools.combinations([acteur.strip("[").strip("]") for acteur in json.loads(ligne)["cast"]], 2))
     return graph
--- test_request.py
+++ test_request.py
@@ -4,9 +4,8 @@ import networkx as nx
 import time
 
 # Q1
-def test_json_vers_nx():
-    t = time.time()
-    G = r.json_vers_nx("../data.txt")
-    print(nx.number_of_edges(G))
-    print(nx.number_of_nodes(G))
-    print(time.time()-t)
+t = time.time()
+G = r.json_vers_nx("../data.txt")
+print(nx.number_of_edges(G))
+print(nx.number_of_nodes(G))
+print(time.time()-t)

dde9fa972705792dd9f02ad92c2c1461b749da88
MignanBaptiste <baptiste.mignan@etu.univ-orleans.fr>
2024-05-27 11:27:42 +0200

    ajout de fichiers

--- test_request.py
+++ test_request.py
@@ -5,7 +5,7 @@ import time
 
 # Q1
 t = time.time()
-G = r.json_vers_nx("../data.txt")
+G = r.json_vers_nx("./data.txt")
 print(nx.number_of_edges(G))
 print(nx.number_of_nodes(G))
 print(time.time()-t)
